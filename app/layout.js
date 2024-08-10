62fc53bd7b62122c816d73a87cd3d05aa29ee8db
Vanshaj Kataria <vanshajkataria3@gmail.com>
2024-08-10 19:50:31 +0530

    Added Dark Mode Function in body tag of layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} h-screen w-screen flex grid-cols-2`}>{children}</body>
+      <body className={`${inter.className} h-screen w-screen flex grid-cols-2 bg-white dark:bg-black text-black dark:text-white`}>{children}</body>
     </html>
   );
 }
